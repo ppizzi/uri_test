e0dd76f48eb1c496b907d79a12a13ca9d67fbe1a
ppizzi <43932233+ppizzi@users.noreply.github.com>
2025-01-29 19:20:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,13 +160,12 @@ if image is not None:
     if action:
         image = Image.open(image).rotate(int(rotate), PIL.Image.NEAREST, expand = 1)
         st.sidebar.image(image)
-
     
-    launch_llm = st.button("Analyze")
-    if launch_llm: 
-        answer=get_LLM_analysis(image, output_language)
-        launch_llm = 0 #reset the button
-        st.write(answer)
+        launch_llm = st.button("Analyze")
+        if launch_llm: 
+            answer=get_LLM_analysis(image, output_language)
+            launch_llm = 0 #reset the button
+            st.write(answer)
 
 
 
