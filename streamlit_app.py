f1be64be19d1e597c54109196d495b14ac359876
ppizzi <43932233+ppizzi@users.noreply.github.com>
2025-01-29 20:00:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ st.sidebar.image("uri_test_reference.jpg")
 
 up_image=st.file_uploader("Upload your photo", type=["jpg","png"])
 if up_image is not None:
-    img_holder = st.sidebar.image(up_image)
+    img_holder = st.image(up_image)
     st.write("Make sure your photo is aligned in the same way as the reference of the test-kit:")
     mapping = {"0":"OK", "90": "90º :arrows_counterclockwise:", "270": "90º :arrows_clockwise:", "180":"180º"}
     rotate = st.radio("Rotate photo: ", ("0","90","270","180"), format_func = lambda x: mapping[x])
