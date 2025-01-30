d5ec20075bcbe7c03b57db8fc3b7f94ee781ccb1
ppizzi <43932233+ppizzi@users.noreply.github.com>
2025-01-30 13:47:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def get_LLM_analysis(model_id, refimageb64, imageb64, language):
                 },
                 {
                     "image": {
-                        "format": "jpg",
+                        "format": "jpeg",
                         "source": {"bytes": refimageb64},
                     }
                 },
@@ -89,7 +89,7 @@ def get_LLM_analysis(model_id, refimageb64, imageb64, language):
                 },
                 {
                     "image": {
-                        "format": "jpg",
+                        "format": "jpeg",
                         "source": {"bytes": imageb64},
                     }
                 }, 
